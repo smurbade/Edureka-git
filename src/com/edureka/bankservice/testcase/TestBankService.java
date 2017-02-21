f760bc00e395b0dc47dcf1e7cd8716419e740df9
sachin murbade <murbade.sachin@gmail.com>
2017-02-20 21:58:51 -0500

    done

--- src/com/edureka/bankservice/testcase/TestBankService.java
+++ src/com/edureka/bankservice/testcase/TestBankService.java
@@ -13,10 +13,8 @@ import javax.servlet.http.HttpSession;
 
 import junit.framework.TestCase;
 
-import junit.Before;
-import junit.Test;
-//import org.junit.Before;
-//import org.junit.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
