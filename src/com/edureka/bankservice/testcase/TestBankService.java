ee748012648aaa4e69a798725c91c0bfddda04b4
sachin murbade <murbade.sachin@gmail.com>
2017-02-20 21:44:52 -0500

    commented and addd lines to testbank.java

--- src/com/edureka/bankservice/testcase/TestBankService.java
+++ src/com/edureka/bankservice/testcase/TestBankService.java
@@ -11,10 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import junit.framework.TestCase;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+//import junit.framework.TestCase;
+
+//import org.junit.Before;
+//import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
